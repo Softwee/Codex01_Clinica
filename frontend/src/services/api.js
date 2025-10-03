5b45626e85f1de93c106ea9ed678b7cafbe547ad
eden verdugo <edenv@difarmer.com>
2025-10-02 17:16:46 -0700

    se correje la direccion de la api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://localhost:57761/'
+  baseURL: import.meta.env.VITE_API_URL || 'https://localhost:57761/api'
 })
 
 api.interceptors.response.use(
